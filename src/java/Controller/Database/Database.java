c691b9780cf805bc0967eeab179d51029d1f4f7e
Gopi Krishna Srungarapu <srungarapugopikrishna@gmail.com>
2014-09-13 08:31:09 +0530

    Completed Login

--- src/java/Controller/Database/Database.java
+++ src/java/Controller/Database/Database.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package Controller.Database;
 
 import Model.VolunteerDetails;
@@ -25,13 +24,14 @@ public class Database {
     private static final String host = "localhost", dbname = "dms", username = "root", password = "root";
     private static final int port = 3306;
     private static String query;
-    
-   public static void getConnected() {
+
+    public static void getConnected() {
         try {
-             Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.jdbc.Driver");
             con = DriverManager.getConnection("jdbc:mysql://" + host + ":" + port + "/" + dbname + "?zeroDateTimeBehavior=convertToNull", username, password);
-            if(con == null)
+            if (con == null) {
                 System.err.println("Unable to Connect");
+            }
         } catch (ClassNotFoundException | SQLException ex) {
             Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -47,24 +47,24 @@ public class Database {
 
     public static int getVolunteerCurrID() {
         getConnected();
-       // System.out.print("getVolunteerCurrID");
-          int VID = -1;
+        // System.out.print("getVolunteerCurrID");
+        int VID = -1;
         try {
             query = "select volunteers_id from dms.volunteers_info ORDER BY volunteers_id DESC LIMIT 1";
-            Statement stmt  = con.createStatement();
-            ResultSet rs=stmt.executeQuery(query);
-            if(rs.next()){
-                
-                while(rs.next()){
-                    VID=rs.getInt("Volunteers_id");
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            if (rs.next()) {
+
+                while (rs.next()) {
+                    VID = rs.getInt("Volunteers_id");
                 }
-            }else{
-            return 0;
+            } else {
+                return 0;
             }
         } catch (SQLException ex) {
             Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
         }
-           
+
         closeConnection();
         return VID;
     }
@@ -72,17 +72,53 @@ public class Database {
     public static void addVolunteerRegistration(VolunteerDetails vdetails) {
         try {
             getConnected();
-              query = "INSERT INTO dms.volunteers_info (`volunteers_id`, name , username, password, contact_no,volunteers_Address)"
-                    + "VALUES (" + ((vdetails.getVolunteerID() + 1)) + ", '" + vdetails.getVolunteerName()+ "', '" + vdetails.getUserNameOfVolunteer()+ "', '" + vdetails.getPassword()+ "', '" + vdetails.getVolunteerContact()+"', '" + vdetails.getVolunteerAddress()+ "')";
+            query = "INSERT INTO dms.volunteers_info (`volunteers_id`, name , username, password, contact_no,volunteers_Address)"
+                    + "VALUES (" + ((vdetails.getVolunteerID() + 1)) + ", '" + vdetails.getVolunteerName() + "', '" + vdetails.getUserNameOfVolunteer() + "', '" + vdetails.getPassword() + "', '" + vdetails.getVolunteerContact() + "', '" + vdetails.getVolunteerAddress() + "')";
             Statement stmt = con.createStatement();
             stmt.executeUpdate(query);
-            
+
             closeConnection();
         } catch (SQLException ex) {
             Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    public String getDisasterType(int Did){
+
+    public static boolean checkLogin(String username, String pwd) {
+
+        System.out.println("In CheckLogin");
+        getConnected();
+        String volunteers_id;
+        try {
+            query = "select volunteers_id from dms.volunteers_info where username='" + username + "'";
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            if (rs.next()) {
+               
+                volunteers_id = rs.getString(1);
+                query = "select password from dms.volunteers_info where volunteers_id='" + volunteers_id + "'";
+                rs = stmt.executeQuery(query);
+                rs.next();
+                String dbpassword = rs.getString(1);
+                 System.out.println("dbpassword---"+dbpassword+"--Pwd---"+pwd);
+                if (dbpassword.equals(pwd)) {
+                    System.out.println("SuccessFull");
+                    closeConnection();
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        closeConnection();
+        return false;
+
+    }
+
+    public String getDisasterType(int Did) {
         getConnected();
         //MyCodeHere
         closeConnection();
---
+++ src/java/Controller/volunteerLogin.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Controller;
+
+import Controller.Database.Database;
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author gopi
+ */
+public class volunteerLogin extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+      //  System.out.println("-*-*-*-In VolunteerLogin-*-*-*-");
+        String username = request.getParameter("username");
+        String pwd = request.getParameter("pwd");
+        RequestDispatcher rd = null;
+       //   System.out.println("UserName-"+username+"    passs--"+pwd);
+         boolean loginsucessfull=Database.checkLogin(username, pwd);
+          if(loginsucessfull){
+       //   System.out.println("Login Successfull");
+          rd = request.getRequestDispatcher("index.html");
+        //  rd = request.getRequestDispatcher("ReadCookies");
+          
+          }
+          else{
+              rd=request.getRequestDispatcher("a.html");
+              //out.print("Name   ");
+          }
+          if(rd!=null)
+            rd.forward(request, response);
+        
+         
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
